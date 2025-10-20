8fd96d8d32cebe2576751f4874dd27756653e153
Risaysky <yasserasklou92@gmail.com>
2025-10-20 13:25:36 +0100

    Implement add button

--- app/components/TodoEditor.tsx
+++ app/components/TodoEditor.tsx
@@ -10,7 +10,9 @@ type todoEditorProps = {
 
 export default function TodoEditor({ todo }: todoEditorProps) {
   const [todoTitle, setTodoTitle] = useState(todo?.title);
-  const [todoList, setTodoList] = useState(todo?.list);
+  const [todoList, setTodoList] = useState(() => {
+    if (todo) return [...todo?.list, { text: "", done: false }];
+  });
   const ref = useRef<HTMLInputElement[]>([]);
 
   function handleText(text: string, index: number) {
@@ -29,10 +31,19 @@ export default function TodoEditor({ todo }: todoEditorProps) {
     });
   }
 
-  function removeItem(index: number) {
+  function addItem() {
     setTodoList((todoList) => {
       const temp = JSON.parse(JSON.stringify(todoList));
-      return temp.filter((_: unknown, i: number) => i !== index);
+      temp.push({ text: "", done: false });
+      return temp;
+    });
+  }
+
+  function removeItem(index: number) {
+    if (index === 0) return;
+    setTodoList((todoList) => {
+      const temp = JSON.parse(JSON.stringify(todoList));
+      return temp.filter((_: unknown, i: number) => i !== index - 1);
     });
   }
 
@@ -46,69 +57,73 @@ export default function TodoEditor({ todo }: todoEditorProps) {
         (e.key === "Backspace" && e.ctrlKey)
       ) {
         e.preventDefault();
-      }
-      const arr = ref.current;
-      const focused = arr.findIndex((element) => element === e.target);
-      if (
-        (e.key === "Enter" && !e.shiftKey && !e.ctrlKey) ||
-        (e.key === "Tab" && !e.shiftKey) ||
-        e.key === "ArrowDown"
-      ) {
-        const index = (focused + 1) % arr.length;
-        const inputLength = arr[index].value.length;
-        arr[index].focus();
-        arr[index].setSelectionRange(inputLength, inputLength);
-      }
-      if (
-        (e.key === "Enter" && e.shiftKey && !e.ctrlKey) ||
-        (e.key === "Tab" && e.shiftKey) ||
-        e.key === "ArrowUp"
-      ) {
-        const index =
-          (((focused > 0 ? focused - 1 : arr.length - 1) % arr.length) +
-            arr.length) %
-          arr.length;
-        const inputLength = arr[index].value.length;
-        arr[index].focus();
-        arr[index].setSelectionRange(inputLength, inputLength);
-      }
-      if (e.key === "Enter" && e.ctrlKey) {
-        if (focused >= 0) handleCheck(focused);
-      }
-      if (e.key === "Backspace" && e.ctrlKey) {
-        if (focused >= 0) removeItem(focused);
+        const arr = ref.current;
+        const focused = arr.findIndex((element) => element === e.target);
+        if ((e.key === "Tab" && !e.shiftKey) || e.key === "ArrowDown") {
+          const index = (focused + 1) % arr.length;
+          const inputLength = arr[index].value.length;
+          arr[index].focus();
+          arr[index].setSelectionRange(inputLength, inputLength);
+        }
+        if ((e.key === "Tab" && e.shiftKey) || e.key === "ArrowUp") {
+          const index =
+            (((focused > 0 ? focused - 1 : arr.length - 1) % arr.length) +
+              arr.length) %
+            arr.length;
+          const inputLength = arr[index].value.length;
+          arr[index].focus();
+          arr[index].setSelectionRange(inputLength, inputLength);
+        }
+        if (e.key === "Enter") {
+          if (focused === todoList?.length) addItem();
+          else if (focused > 0) handleCheck(focused - 1);
+        }
+        if (e.key === "Backspace" && e.ctrlKey) {
+          if (focused > 0) removeItem(focused);
+        }
       }
     }
     window.addEventListener("keydown", handleKey);
     return () => window.removeEventListener("keydown", handleKey);
-  }, []);
+  }, [todoList?.length]);
 
   return (
     <div className="px-5 pt-3">
       <input
+        ref={(node) => {
+          const arr = ref.current;
+          arr[0] = node!;
+          return () => {
+            arr.splice(0, 1);
+          };
+        }}
         value={todoTitle ?? "Untitled"}
         onChange={(e) => setTodoTitle(e.target.value)}
-        className={`text-2xl text-gray-200 placeholder:text-gray-500`}
+        className={`field-sizing-content pe-4 text-2xl text-gray-200 placeholder:text-gray-500`}
       />
-      <ul className="flex h-full flex-col py-3">
-        {todoList?.map((item, index) => (
-          <TodoItem
-            key={index}
-            ref={(node) => {
-              const arr = ref.current;
-              arr[index] = node!;
-              return () => {
-                arr.splice(index, 1);
-              };
-            }}
-            index={index}
-            text={item.text}
-            checked={item.done}
-            onText={handleText}
-            onCheck={handleCheck}
-            removeItem={removeItem}
-          />
-        ))}
+      <ul className="flex h-full flex-col items-start py-3">
+        {todoList?.map((item, index) => {
+          return (
+            <TodoItem
+              key={index}
+              ref={(node) => {
+                const arr = ref.current;
+                arr[index + 1] = node!;
+                return () => {
+                  arr.splice(index + 1, 1);
+                };
+              }}
+              index={index}
+              text={item.text}
+              checked={item.done}
+              onText={handleText}
+              onCheck={handleCheck}
+              removeItem={removeItem}
+              addItem={addItem}
+              type={index !== todoList.length - 1 ? "normal" : "add-button"}
+            />
+          );
+        })}
       </ul>
     </div>
   );
--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -8,6 +8,8 @@ type todoItemProps = {
   onText: (text: string, index: number) => void;
   onCheck: (index: number) => void;
   removeItem: (index: number) => void;
+  addItem: () => void;
+  type: "normal" | "add-button";
 };
 
 export default function TodoItem({
@@ -18,6 +20,8 @@ export default function TodoItem({
   onText,
   onCheck,
   removeItem,
+  addItem,
+  type,
 }: todoItemProps) {
   return (
     <li className="space-x-1.5">
@@ -25,21 +29,30 @@ export default function TodoItem({
         type="checkbox"
         checked={checked}
         onChange={() => onCheck(index)}
+        disabled={type === "add-button"}
       />
       <input
         ref={ref}
-        className="field-sizing-content pe-2"
+        className={`${checked && "text-gray-200/70 line-through"} field-sizing-content min-w-20 pe-2 placeholder-gray-500`}
         type="text"
         value={text}
-        onChange={(e) => onText(e.target.value, index)}
-        onBlur={() => (text ? "" : removeItem(index))}
+        placeholder={type === "add-button" ? "Add a new item" : ""}
+        onChange={(e) => {
+          onText(e.target.value, index);
+          if (type === "add-button") addItem();
+        }}
+        onBlur={() => {
+          if (type === "normal" && !text) removeItem(index + 1);
+        }}
       />
-      <button
-        className="h-4 w-4 cursor-pointer content-center bg-gray-800 text-center text-xs text-gray-500 opacity-25 hover:opacity-100"
-        onClick={() => removeItem(index)}
-      >
-        x
-      </button>
+      {type === "normal" && (
+        <button
+          className="h-4 w-4 cursor-pointer content-center bg-gray-800 text-center text-xs text-gray-500 opacity-25 hover:opacity-100"
+          onClick={() => removeItem(index + 1)}
+        >
+          x
+        </button>
+      )}
     </li>
   );
 }
